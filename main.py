8434d837f4f2def265e677fc4726fd183293dd0d
Starim <slixbits@gmail.com>
2012-07-29 14:51:49 -0700

    Set some useful default behaviors in the main Python script.
    Specifically, the main script now accepts UTF-8 characters and performs floating-point (instead of integer) division by default.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# make Python do floating-point division by default
+from __future__ import division
 
 from google.appengine.ext import db
 from lib import web
