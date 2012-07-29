45258a9634ef446c87a4fb2bc8f04ac1e58954cc
Starim <slixbits@gmail.com>
2012-07-29 14:57:51 -0700

    Add a models folder for DB models with a stub for the autobidder model.

---
+++ models/autobidder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# make Python do floating-point division by default
+from __future__ import division
+
+from google.appengine.ext import db
+from lib import web
+
+class autobidder:
+
