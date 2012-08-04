3ec9b9b8d942a17df70d24bdaefc0535b37cb498
Starim <slixbits@gmail.com>
2012-08-04 15:31:06 -0700

    Create an AppSettings class to allow access to important global settings.

---
+++ models/app_settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# make Python do floating-point division by default
+from __future__ import division
+
+
+class AppSettings(db.Model):
+	''' This class stores important global settings for the auction system. '''
+
+	# password hashing settings
+	OLD_HASH_WORK_LEVEL = 12
+	NEW_HASH_WORK_LEVEL = 12
+
+	# the time added to an auction's duration when a bid is placed, in seconds
+	AUCTION_BID_BUMP_TIME = 120
+
+	# the time threshold on an auction below which the autobidder will place bids, in seconds
+	AUTOBIDDER_TIME_THRESHOLD = 5
