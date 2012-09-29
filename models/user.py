478b11e9ef4b2f303fcd2d69c7b36d22c11f2879
Darin Hoover <darin@hoover.fm>
2012-09-28 19:03:38 -0700

    Added email_validated and email_validation_code to User model.

--- models/user.py
+++ models/user.py
@@ -10,6 +10,8 @@ class User(db.Model):
 	password_salt = db.StringProperty(required=True)
 	create_time = db.DateTimeProperty(required=True)
 	personal_information = db.StringProperty()
+	email_validated = db.BooleanProperty(default=False)
+	email_validation_code = db.StringProperty(required=True)
 	# implicit property 'active_autobidders' created by the Autobidder class
 	# implicit property 'auctions_won' created by the Auction class
 	# implicit property 'past_bids' created by the BidHistory class
