6566a257c0999f2afa98c160c0d7bf9cb6cb1e15
Darin Hoover <darin@hoover.fm>
2012-10-05 10:16:18 -0700

    Updated auction model with latest changes from items-and-auctions

--- models/auction.py
+++ models/auction.py
@@ -6,30 +6,45 @@ from __future__ import division
 
 from threading import Timer
 from google.appengine.ext import db
-import models.item as item
-import models.user as user
+from models import item, user, decimal_property
+import datetime
+from datetime import timedelta
 
 class Auction(db.Model):
 	''' This class represents a single auction. '''
-
-	id = db.IntegerProperty(required=True)
-	name = db.StringProperty(required=True)
 	item = db.ReferenceProperty(item.Item, collection_name='auctions')
-	currentPrice = decimal.DecimalProperty(default='0.0')
-	currentWinner = db.ReferenceProperty(user.User, collection_name='auctions_won')
-	auctionEnd = db.DateTimeProperty()
+	current_price = decimal_property.DecimalProperty(default="0.00")
+	current_winner = db.ReferenceProperty(user.User, collection_name='auctions_won')
+	auction_end = db.DateTimeProperty()
 	# implicit property 'attached_autobidders' created by the Autobidder class
 	# implicit property 'past_bids' created by the BidHistory class
 
-	def __init__(self):
+	@staticmethod
+	def get_by_ids(ids):
+		'''
+			Generates a list of auctions whose id is contained in the {ids} list
+		'''
+		ids = [map(int, x) for x in ids]
+
+		print ids[0]
+		return Auction.all().filter("id IN", ids).get()
+
+	@staticmethod
+	def get_current(count=10):
+		'''
+			Lists the top {count=10} active auctions
+		'''
+		#return Auction.all().filter("auction_end < " .order("auction_end").fetch(count)
+
+	#def __init__(self):
 
 		# initialize the auction timer, but do not start it
-		self.countdown_timer = Timer(
-			AppSettings().AUCTION_INITIAL_DURATION,
-			AuctionController.invoke_auto_bidding,
-			(self)
-		)
+		#self.countdown_timer = Timer(
+		#	AppSettings().AUCTION_INITIAL_DURATION,
+		#	AuctionController.invoke_auto_bidding,
+		#	(self)
+		#)
 
-	def begin(self):
-		''' Begins this auction. '''
-		self.countdown_timer.start()
+	#def begin(self):
+		#''' Begins this auction. '''
+		#self.countdown_timer.start()
