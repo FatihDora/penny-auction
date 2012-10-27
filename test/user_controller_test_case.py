e9b191f39c3803ef8ef533d1e3b173b5ac3fb4a4
Kevin Mershon <nwlinkvxd@gmail.com>
2012-10-27 14:57:06 +0000

    Fixed broken unit tests and removed invalid ones

--- test/user_controller_test_case.py
+++ test/user_controller_test_case.py
@@ -8,97 +8,85 @@ from google.appengine.ext import db
 from google.appengine.ext import testbed
 
 class UserControllerTestCase(unittest.TestCase):
-    def setUp(self):
-        self.testbed = testbed.Testbed()
-        self.testbed.activate()
-        self.testbed.init_datastore_v3_stub()
+	def setUp(self):
+		self.testbed = testbed.Testbed()
+		self.testbed.activate()
+		self.testbed.init_datastore_v3_stub()
 
-    def tearDown(self):
-        self.testbed.deactivate()
+	def tearDown(self):
+		self.testbed.deactivate()
 
-    def make_user(self):
-        user_controller.user_register("testUser", "testUser@me.com",
-                "testPassword")
+	def make_user(self):
+		user_controller.user_register("test", "user", "testUser", "testUser@me.com",
+				"testPassword")
 
-    def testUserAuthenticate(self):
-        self.make_user()
+	def testUserAuthenticate(self):
+		self.make_user()
 
-        # bad username
-        try:
-            user_controller.user_authenticate("fakeUser", "doesn't matter")
-            self.fail("User permitted to login with bad username!")
-        except:
-            # expected behavior
-            pass
+		# bad username
+		try:
+			user_controller.user_authenticate("fakeUser", "doesn't matter")
+			self.fail("User permitted to login with bad username!")
+		except:
+			# expected behavior
+			pass
 
-        # bad password
-        try:
-            user_controller.user_authenticate("testUser", "bad password")
-            self.fail("User permitted to login with bad password!")
-        except:
-            # expected behavior
-            pass
+		# bad password
+		try:
+			user_controller.user_authenticate("testUser", "bad password")
+			self.fail("User permitted to login with bad password!")
+		except:
+			# expected behavior
+			pass
 
 # cannot run this part because the cookie cannot be set due to path resolution
 # problems in web.py's setcookie function
 #
-#       # good login
-#       user_hash = user_controller.user_authenticate("testUser",
-#           "testPassword")
-#       user_key = db.Key.from_path("User", "testUser")
-#       user_object = db.get(user_key)
-#       hashed_password = user_controller.user_hash_password("testUser",
-#           "testPassword", user_object.password_salt)
-#       self.assertEquals(hashed_password, user_hash)
-#       user_key = db.Key.from_path("User", "testUser")
-#       user_object = db.get(user_key)
-#       hashed_password = user_controller.user_hash_password("testUser",
-#           "testPassword", user_object.password_salt)
+#		# good login
+#		user_hash = user_controller.user_authenticate("testUser",
+#			"testPassword")
+#		user_key = db.Key.from_path("User", "testUser")
+#		user_object = db.get(user_key)
+#		hashed_password = user_controller.user_hash_password("testUser",
+#			"testPassword", user_object.password_salt)
+#		self.assertEquals(hashed_password, user_hash)
+#		user_key = db.Key.from_path("User", "testUser")
+#		user_object = db.get(user_key)
+#		hashed_password = user_controller.user_hash_password("testUser",
+#			"testPassword", user_object.password_salt)
 
-    def testUserCannotRegisterTwice(self):
-        self.make_user()
-        try:
-            self.make_user()
-            self.fail("Repeat registration was permitted!")
-        except:
-            # expected behavior
-            pass
+	def testUserCannotRegisterTwice(self):
+		self.make_user()
+		try:
+			self.make_user()
+			self.fail("Repeat registration was permitted!")
+		except:
+			# expected behavior
+			pass
 
-    def testUserEmailExists(self):
-        self.assertFalse(user_controller.user_email_exists("testUser@me.com"))
+	def testUserUpdatePassword(self):
+		self.make_user()
 
-        self.make_user()
-        self.assertTrue(user_controller.user_email_exists("testUser@me.com"))
+		# validate the password hash
+		user_key = db.Key.from_path("User", "testUser")
+		user_object = db.get(user_key)
+		hashed_password = user_controller.user_hash_password("testUser",
+			"testPassword", user_object.password_salt)
+		self.assertEquals(hashed_password, user_object.hashed_password)
 
-    def testUserUsernameExists(self):
-        self.assertFalse(user_controller.user_username_exists("testUser"))
+		# change the password
+		user_controller.user_update_password(user_object, "testNewPassword")
 
-        self.make_user()
-        self.assertTrue(user_controller.user_username_exists("testUser"))
+		# validate the new password hash
+		new_hashed_password = user_controller.user_hash_password("testUser",
+			"testNewPassword", user_object.password_salt)
+		self.assertEquals(new_hashed_password, user_object.hashed_password)
 
-    def testUserUpdatePassword(self):
-        self.make_user()
-
-        # validate the password hash
-        user_key = db.Key.from_path("User", "testUser")
-        user_object = db.get(user_key)
-        hashed_password = user_controller.user_hash_password("testUser",
-            "testPassword", user_object.password_salt)
-        self.assertEquals(hashed_password, user_object.hashed_password)
-
-        # change the password
-        user_controller.user_update_password(user_object, "testNewPassword")
-
-        # validate the new password hash
-        new_hashed_password = user_controller.user_hash_password("testUser",
-            "testNewPassword", user_object.password_salt)
-        self.assertEquals(new_hashed_password, user_object.hashed_password)
-
-        # pull a new refrence to the user from the DB and re-validate the new
-        # password hash
-        dup_user_key = db.Key.from_path("User", "testUser")
-        dup_user_object = db.get(dup_user_key)
-        self.assertEquals(new_hashed_password, dup_user_object.hashed_password)
+		# pull a new refrence to the user from the DB and re-validate the new
+		# password hash
+		dup_user_key = db.Key.from_path("User", "testUser")
+		dup_user_object = db.get(dup_user_key)
+		self.assertEquals(new_hashed_password, dup_user_object.hashed_password)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
