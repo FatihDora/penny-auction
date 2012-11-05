19ca59fe2e6c1c2afcabb72cef7221cdb6c7a71c
Kevin Mershon <nwlinkvxd@gmail.com>
2012-11-05 00:25:08 +0000

    Started working on the item controller tests

---
+++ test/item_controller_test_case.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import controllers.item_controller as item_controller
+import fixtures.dummy_items as dummy_items
+import unittest
+
+from google.appengine.ext import db
+from google.appengine.ext import testbed
+
+class ItemControllerTestCase(unittest.TestCase):
+	def setUp(self):
+		self.testbed = testbed.Testbed()
+		self.testbed.activate()
+		self.testbed.init_datastore_v3_stub()
+
+		# make some fixture items
+		dummy_items.DummyItems.setup()
+
+	def tearDown(self):
+		self.testbed.deactivate()
+
+	def make_item(self):
+		# make an item
+		item_controller.ItemController.item_add("Gorilla Munch", 9001, "14.99",
+			"http://thereisnoneed.com", "http://i.imgur.com/1jqAT.png")
+
+	def testCreateItem(self):
+
+		# verify the item doesn't exist
+		all_items = item_controller.ItemController.items_list()
+		for item in all_items:
+			if (item.name == "Gorilla Munch"):
+				self.fail("Impossible! Gorilla Munch already exists!")
+				return
+
+		# make the item
+		self.make_item()
+
+		# verify the item exists
+		found = False
+		all_items = item_controller.ItemController.items_list()
+		for item in all_items:
+			if (item.name == "Gorilla Munch"):
+				success = True
+		if not (success):
+			self.fail("Failed to create item 'Gorilla Munch'")
+
+	def testCannotCreateDuplicateItems(self):
+		self.fail("implement me")
+
+	def testItemAccessorsWhenItemDoesNotExist(self):
+		self.fail("implement me")
+
+	def testItemAccessorsWhenItemExists(self):
+		self.fail("implement me")
+
+
