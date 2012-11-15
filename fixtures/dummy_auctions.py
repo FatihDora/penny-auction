f99bd2d9e144e470a74527e5feefa934dd1df525
Brent Houghton <slixbits@gmail.com>
2012-11-15 14:53:19 -0800

    Update the autobidder to use the new generic InsufficientBidsException class instead of the old autobidder-specific NoBidsRemainingExcpetion.

--- fixtures/dummy_auctions.py
+++ fixtures/dummy_auctions.py
@@ -19,8 +19,9 @@ class DummyAuctions(object):
 		db.delete(auction.Auction.all())
 		
 		items = ['MacBook Air', 'MacBook Pro', 'Airport Express']
-		delay = timedelta(0)
+		delay = timedelta(seconds=10)
 		for x in range(0, 100):
-			delay += timedelta(days=0.1)
 			item_name = items[random.randint(0,2)]
-			auction_controller.AuctionController.create(item_name=item_name, start_delay=delay)
+			auction_controller.AuctionController.create(item_name=item_name, start_delay=delay, bid_pushback_time=datetime.timedelta(hours=1))
+			delay += timedelta(days=0.1)
+
