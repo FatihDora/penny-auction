206c5481cd325479e25f34168239220c21eb4d5c
Brent Houghton <slixbits@gmail.com>
2013-02-18 17:00:41 -0800

    Implement working login tests from the API perspective, instead of testing individual methods in the user controller.

--- test/user_controller_test_case.py
+++ test/user_controller_test_case.py
@@ -8,15 +8,20 @@ from __future__ import unicode_literals
 
 import models.user as user
 import controllers.user_controller as user_controller
-import unittest
 
 import lib.bcrypt.bcrypt as bcrypt
-from lib import web
+
+import unittest
+import urllib
+import urllib2
+import cookielib
+import json
 
 from google.appengine.ext import db
 from google.appengine.ext import testbed
 
 class UserControllerTestCase(unittest.TestCase):
+
 	def setUp(self):
 		self.testbed = testbed.Testbed()
 		self.testbed.activate()
@@ -31,61 +36,86 @@ class UserControllerTestCase(unittest.TestCase):
 		user_object = user.User.get_by_username("testUser")
 		self.assertNotEquals(None, user_object, "Failed to create User!")
 
-	def testPersonaAuthentication(self):
+	def test_failed_login(self):
+		''' Test failed login.'''
+
 		self.make_user()
 
-		# test failed login
+		# instruct the user controller to fail this authentiation attempt
 		user_controller.UserController._PERSONA_AUTH_URL = "mock:failure"
-		assertion = "fake assertion"
-		expected_result = None
-		actual_result = user_controller.UserController.persona_login(assertion)
+
+		request_parameters = {"assertion": "fake assertion"}
+		expected_response = {"result": False, "username": None}
+		cookie_jar = cookielib.CookieJar()
+		reader = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+		encoded_request_parameters = urllib.urlencode(request_parameters)
+		raw_response = reader.open("http://localhost:8080/persona_login?{}".format(encoded_request_parameters))
+		actual_response = json.loads(raw_response.read())
 
 		# check return value
-		if actual_result != expected_result:
+		if actual_response != expected_response:
 			self.fail(
-				"""persona_login() did not return the expected value for an invalid login.
+				"""/persona_login did not return the expected value for an invalid login.
 				Expected: {expected!r}
 				Actual: {actual!r}""".format(
-					expected=expected_result,
-					actual=actual_result
+					expected=expected_response,
+					actual=actual_response
 				)
 			)
 
 		# check that no session cookie was set
-		if web.cookies().get(UserController._COOKIE_NAME):
-			self.fail("User logged in with invalid credentials and got a session cookie set.")
-		
+		for cookie in cookie_jar:
+			if cookie.key == user_controller.UserController._COOKIE_NAME:
+				self.fail("User logged in with invalid credentials and got a session cookie.")
 
 
+	def test_successful_login(self):
+		''' Test successful login with existing user. '''
 
-		# test successful login with existing user
+		self.make_user()
+
+		# instruct the user controller to pass this authentiation attempt
 		user_controller.UserController._PERSONA_AUTH_URL = "mock:success:testUser@me.com"
-		assertion = "fake assertion"
-		expected_result = User.get_by_email("testUser@me.com")
-		actual_result = user_controller.UserController.persona_login(assertion)
+
+		request_parameters = {"assertion": "fake assertion"}
+		expected_response = {"result": True, "username": "testUser"}
+		cookie_jar = cookielib.CookieJar()
+		reader = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+		encoded_request_parameters = urllib.urlencode(request_parameters)
+		raw_response = reader.open("http://localhost:8080/persona_login?{}".format(encoded_request_parameters))
+		actual_response = json.loads(raw_response.read())
 
 		# check return value
-		if actual_result != expected_result:
+		if actual_response != expected_response:
 			self.fail(
 				"""persona_login() did not return the expected value for an expected successful login of pre-existing user.
 				Expected: {expected!r}
 				Actual: {actual!r}""".format(
-					expected=expected_result,
-					actual=actual_result
+					expected=expected_response,
+					actual=actual_response
 				)
 			)
 
-		expected_cookie_contents = bcrypt.hashpw(assertion + "testUser@me.com", bcrypt.gensalt())
-		actual_cookie_contents = web.cookies().get(UserController._COOKIE_NAME)
-		if actual_cookie_conents is None:
+		# check that the correct session cookie was set
+		session_cookie = None
+		for cookie in cookie_jar:
+			if cookie.key == user_controller.UserController._COOKIE_NAME:
+				session_cookie = cookie
+
+		if not session_cookie:
 			self.fail("User logged in with valid credentials but no session cookie was set.")
-		elif cookie_contents != expected_cookie_contents:
+
+		db_cookie = UserCookie.all().filter("username", "testUser").get()
+		if not db_cookie:
+			self.fail("No session cookie was saved to the database for the intended user after a successful login.")
+		session_token = db_cookie.token
+		if session_token != session_cookie.value:
 			self.fail(
-				"""User logged in successfully but session cookie contents don't match expected contents.
+				"""User logged in successfully but session cookie contents don't match session token.
 				Expected: {expected!r}
 				Actual: {actual!r}""".format(
-					expected=expected_cookie_contents,
-					actual=cookie_contents
+					expected=session_token,
+					actual=session_cookie.value
 				)
 			)
 
